c9cf301d2df138a6003cde8cd732b3956d9fd0a7
danishRajawat <ibnfarooq070@gmail.com>
2025-09-05 21:59:11 +0530

    blog list added

--- client/src/components/HomeComponents/BlogList.jsx
+++ client/src/components/HomeComponents/BlogList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { blogCategories } from "../../assets/assets";
+import { blog_data, blogCategories } from "../../assets/assets";
 import { motion } from "motion/react";
+import BlogCard from "./BlogCard";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
@@ -27,7 +28,10 @@ const BlogList = () => {
           </div>
         ))}
       </div>
-      <div>{/* ---- blog cards ---- */}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {/* ---- blog cards ---- */}
+{blog_data.filter((blog)=> menu === "All" ? true : blog.category === menu).map((blog) => <BlogCard blog={blog} key={blog._id}/>)}
+      </div>
     </div>
   );
 };
