d40ec2b4b92e01d12c7fa9d79e5a7ab9eb044f0d
danishRajawat <ibnfarooq070@gmail.com>
2025-09-06 22:24:58 +0530

    admin routes setup and files added and navbar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/Addblog";
+import ListBlog from "./pages/admin/Listblog";
+import Comments from "./pages/admin/Comments";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-blog" element={<AddBlog />} />
+          <Route path="list-blog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
