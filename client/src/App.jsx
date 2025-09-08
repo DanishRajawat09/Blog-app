61c6e837e330bec3447171773e24d4d4b22c408c
danishRajawat <ibnfarooq070@gmail.com>
2025-09-08 20:08:46 +0530

    login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,14 @@ import Dashboard from "./pages/admin/Dashboard";
 import AddBlog from "./pages/admin/Addblog";
 import ListBlog from "./pages/admin/Listblog";
 import Comments from "./pages/admin/Comments";
+import Login from "./components/adminComponents/Login";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={<Layout />}>
+        <Route path="/admin" element={false ?<Layout/> : <Login/>}>
           <Route index element={<Dashboard />} />
           <Route path="add-blog" element={<AddBlog />} />
           <Route path="list-blog" element={<ListBlog />} />
