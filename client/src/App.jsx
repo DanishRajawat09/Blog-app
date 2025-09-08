961eb47d75f015a970e703279c27840573babcce
danishRajawat <ibnfarooq070@gmail.com>
2025-09-08 21:56:00 +0530

    dashboard header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={false ?<Layout/> : <Login/>}>
+        <Route path="/admin" element={true ?<Layout/> : <Login/>}>
           <Route index element={<Dashboard />} />
           <Route path="add-blog" element={<AddBlog />} />
           <Route path="list-blog" element={<ListBlog />} />
