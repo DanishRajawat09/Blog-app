e980dd4d1d372e50366d6c6878aef22ab6a21b33
danishRajawat <ibnfarooq070@gmail.com>
2025-09-20 18:08:34 +0530

    delete blog and togglePublished api integreated

--- client/src/components/adminComponents/BlogTableItem.jsx
+++ client/src/components/adminComponents/BlogTableItem.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const BlogTableItem = ({ blog, fetchBlogs, index }) => {
+  const { axios } = useAppContext();
+
   const { title, createdAt } = blog;
   const BlogDate = new Date(createdAt);
+
+  const deleteBlog = async () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this blog?"
+    );
+
+    if (!confirm) {
+      return;
+    }
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/blog/delete-blog/${blog._id}`
+      );
+
+      if (data.success) {
+        toast.success("blog deleted successfully");
+        await fetchBlogs();
+      } else {
+        toast.error("error while deleting blog");
+      }
+    } catch (err) {
+      console.log("delete blog request error", err);
+    }
+  };
+
+  const togglePublished = async () => {
+    try {
+      const { data } = await axios.patch("/api/v1/blog/ispublished", {
+        id: blog._id,
+      });
+      if (data.success) {
+        await fetchBlogs();
+      } else {
+        toast.error("error while deleting blog");
+      }
+    } catch (err) {
+      console.log("toggle published request error", err);
+    }
+  };
+
   return (
     <tr className="border-y border-gray-300">
       <th className="px-2 py-4">{index}</th>
@@ -19,10 +63,14 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
         </p>
       </td>
       <td className="px-2 py-4 flex text-xs gap-3">
-        <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+        <button
+          onClick={togglePublished}
+          className="border px-2 py-0.5 mt-1 rounded cursor-pointer"
+        >
           {blog.isPublished ? "Unpublish" : "Publish"}
         </button>
         <img
+          onClick={deleteBlog}
           src={assets.cross_icon}
           alt=""
           className="w-8 hover:scale-110 transition-all cursor-pointer"
