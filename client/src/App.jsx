be8439d1a1cb8499f3b2d3f151604986fb1ce538
danishRajawat <ibnfarooq070@gmail.com>
2025-10-05 02:38:21 +0530

    app.jsx addblog page route issue solve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,28 +3,31 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
-import AddBlog from "./pages/admin/Addblog.jsx";
+import AddBlog from "./pages/admin/Addblog";
 import ListBlog from "./pages/admin/Listblog";
 import Comments from "./pages/admin/Comments";
-import "quill/dist/quill.snow.css"
-import {Toaster} from "react-hot-toast"
+import "quill/dist/quill.snow.css";
+import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Signp from "./components/adminComponents/Signup";
 import Login from "./components/adminComponents/Login";
 const App = () => {
-
-
   return (
     <div>
-      <Toaster/>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signp />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={<ProtectedRoute>
-          <Layout/>
-        </ProtectedRoute>}>
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Dashboard />} />
           <Route path="add-blog" element={<AddBlog />} />
           <Route path="list-blog" element={<ListBlog />} />
