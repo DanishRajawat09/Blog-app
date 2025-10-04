d1593a6abb3cb20118c18ac311509da9bd5ed892
danishRajawat <ibnfarooq070@gmail.com>
2025-10-05 04:16:25 +0530

    cors origin correction

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import { errorHandler } from "./utils/errorHandler.js";
 const app = express();
 
 // middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandler)
