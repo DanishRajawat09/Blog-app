11be1e5504ccac6c87587893e6a33e20076cdeaa
danishRajawat <ibnfarooq070@gmail.com>
2025-10-05 11:10:54 +0530

    resolve db connection

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,13 +1,16 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 async function connect() {
-    try {
-     await mongoose.connect(`${process.env.MONGODB_URI}/${"D-BLOG"}`)
-
-    } catch (error) {
-        console.error(error.message)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${"D-BLOG"}`, {
+     serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      maxIdleTimeMS: 60000,
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 }
 
-export default connect
+export default connect;
