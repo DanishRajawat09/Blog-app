bbc9be4be08c4a6894f182d2d158495495bf9a89
danishRajawat <ibnfarooq070@gmail.com>
2025-10-05 12:04:38 +0530

    change the mongodb connection

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,22 +1,21 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
 async function connect() {
-  if (isConnected) {
-    // Reuse existing connection
-    console.log("🟢 Using existing MongoDB connection");
-    return;
-  }
   try {
-   const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${"D-BLOG"}`, {
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      maxIdleTimeMS: 60000,
-    });
-    isConnected = true;
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    mongoose.connection.on("connected", () =>
+      console.log("database is connected")
+    );
+    const conn = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${"D-BLOG"}`,
+      {
+        //   serverSelectionTimeoutMS: 5000,
+        //   socketTimeoutMS: 45000,
+        //   maxIdleTimeMS: 60000,
+      }
+    );
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -8,26 +8,25 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./utils/errorHandler.js";
 const app = express();
 
+await connect(); // only first cold start actually connects
+
 // middlewares
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(errorHandler)
+app.use(errorHandler);
 // Routes
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/blog", blogRouter);
 const PORT = process.env.PORT || 3000;
 
-
 const startServer = async () => {
   try {
-    await connect(); // only first cold start actually connects
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${process.env.PORT || 5500}`);
     });
   } catch (err) {
     console.error(err);
-    process.exit(1);
   }
 };
 
